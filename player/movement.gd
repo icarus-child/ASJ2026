899ed48cbcc03c016198446f7bdeef6b2cda441e
Daniel Doran <ddoran3@gmail.com>
2026-05-23 20:04:10 -0600

    cap player acceleration

--- player/movement.gd
+++ player/movement.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 @export var max_speed: float
+@export var max_accel: float
 
-func _physics_process(_delta: float) -> void:
-	var move_input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = move_input * max_speed
-	
+func _physics_process(delta: float) -> void:
+	var move_input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down").limit_length()
+	var target_vel: Vector2 = move_input * max_speed
+	velocity += (target_vel - velocity).limit_length(max_accel * delta)
 	move_and_slide()
+	velocity = get_real_velocity()
