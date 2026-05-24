c0c973cd348eb24632f770e34865197b54b5c120
Ethan Meier <e652@hotmail.ca>
2026-05-23 21:48:42 -0700

    basic enemy system

---
+++ enemies/test_enemy.gd
@@ -0,0 +1,72 @@
+extends Node2D
+
+# Notes for self
+# When attack off cooldown, calculate optimal attack based on current location
+#     'optimal' attack prioritizes shortest range attack that will hit
+# Calculate path to optimal range for this enemy
+# Attack when off cooldown, move when on cooldown
+
+# NOTE: AI should try to avoid being cornered -> handle during pathfinding logic
+# NOTE: AI might attack less when the player has a lot of spells to make themselves harder to hit?
+
+@export var min_attack_cooldown: float = 3
+@export var max_attack_cooldown: float = 6
+
+var can_attack: bool = true
+
+@onready var test_projectile := preload("res://enemies/test_projectile.tscn") as PackedScene
+@onready var attacks: Array[Attack] = [
+	Attack.new(100, func(player_ref: CharacterBody2D) -> void: _shoot_projectile(player_ref))
+]
+@onready var max_attack_range: float = (
+	attacks.map(func(attack: Attack) -> float: return attack.attack_range).max()
+)
+@onready var player: CharacterBody2D = get_parent().get_node("Player")
+
+
+func _ready() -> void:
+	# sort ascending by attack range
+	attacks.sort_custom(
+		func(a1: Attack, a2: Attack) -> bool: return a1.attack_range < a2.attack_range
+	)
+
+
+class Attack:
+	var attack_range: float
+	var attack_action: Callable
+
+	func _init(range_in: float, action_in: Callable) -> void:
+		attack_range = range_in
+		attack_action = action_in
+
+
+func _process(_delta: float) -> void:
+	var distance_to_player := _get_distance_to_player()
+	if can_attack and distance_to_player <= max_attack_range:
+		_calculate_optimal_attack(distance_to_player).attack_action.bind(player).call()
+		var timer := Timer.new()
+		timer.one_shot = true
+		timer.autostart = true
+		timer.wait_time = randf_range(min_attack_cooldown, max_attack_cooldown)
+		timer.timeout.connect(func() -> void: can_attack = true)
+		can_attack = false
+		add_child(timer)
+
+
+# calculate shortest range attack that is within the attack range
+func _calculate_optimal_attack(range_to_player: float) -> Attack:
+	return attacks[attacks.find_custom(
+		func(attack: Attack) -> bool: return attack.attack_range <= range_to_player
+	)]
+
+
+func _get_distance_to_player() -> float:
+	return 10
+
+
+# TODO: make projectile max speed owned by the projectile
+func _shoot_projectile(target: Node2D) -> void:
+	var projectile := test_projectile.instantiate() as RigidBody2D
+	add_sibling(projectile)
+	projectile.position = position
+	projectile.linear_velocity = position.direction_to(target.position) * 400
